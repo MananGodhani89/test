b7a507ba3069dd7c9e6726e47c8ee2dda4933985
MananGodhani89 <130462891+MananGodhani89@users.noreply.github.com>
2023-05-26 18:30:46 +0530

    test1.c

--- test1.c
+++ test1.c
@@ -1,26 +1,33 @@
 #include<stdio.h>
 
-
-
 main()
-{
-	int a=50,b=150,c=250,d=300,sum;
-	
-	printf("for first 50 unit rs=0.50\n");
-	sum=a/0.50;
-	printf("%d\n",sum);
-	
-	printf("for first 150 unit rs=0.75\n");
-	sum=b/0.75;
-	printf("%d\n",sum);
-	
-	printf("for first 250 unit rs=1.00\n");
-	sum=c/1.00;
-	printf("%d\n",sum);
-	
-	printf("for first 250 above unit rs=1.50\n");	
-	sum=d/1.50;
-	printf("%d\n",sum);
 
+{
+	int Unit;
+	float Amount,T_A,Surch;
+	printf("Enter  Total Units Value =");
+	scanf("%d",&Unit);
 	
+	
+	
+	if(Unit<=50)
+	{
+		Amount=Unit*0.50;
+	}
+	else if(Unit<=150)
+	{
+		Amount=Unit*25+(Unit-50)*0.75;
+	}
+	else if(Unit<=250)
+	{
+		Amount=100+(Unit-250)*1.20;
+	}
+	else
+	{
+		Amount=220+(Unit-250)*1.50;
+	}
+	Surch=Amount*0.20;
+	T_A=Amount+Surch;
+	
+	printf("YOUR ELECTRICITY BILL=%20f",T_A);
 }
